73899d563a1075b19d7e393eddb80062a999416b
Roshanshrivas <roshanshrivas11@gmail.com>
2024-07-04 16:49:16 +0530

    update

--- server/connection.js
+++ server/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  const connection = await mongoose.connect(process.env.MONGODB_URL);
+  const connection = await mongoose.connect(process.env.MONGODB_URL, {
+      ssl: true,
+      sslValidate: true,
+      tlsAllowInvalidCertificates: false
+  });
   if (connection.STATES.uninitialized) {
     console.log("Database is not connected");
   }
