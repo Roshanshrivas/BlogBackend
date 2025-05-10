33fa90411c9d3333be4b19579718e898b8ece077
Roshanshrivas <roshanshrivas11@gmail.com>
2025-05-10 12:48:38 +0530

    index.js changes

--- server/index.js
+++ server/index.js
@@ -19,9 +19,10 @@ app.use((req, res, next) => {
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
-
 
 
 
